9aa0b5514088d50a5e6886281f23377af62b3673
me4 <abduhamid.sultonov@gmail.com>
2017-09-25 13:45:23 +0300

    find close brackets

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ module.exports = function check(str, bracketsConfig) {
   if ((str.length % 2 !== 0) || (str.length == 0)){
   	return false;
   }
+  function findCloseBracket(openBracket, bracketsConfig) {
+        var result;
+        bracketsConfig.forEach(function(item) {
+            if (openBracket == item[0]) result = item[1]
+        }); 	
+
+        return result;
+   };
+   
 }
