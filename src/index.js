c800ee6f3f9163e3c819d7e9987ff27335756ecf
me4 <abduhamid.sultonov@gmail.com>
2017-09-25 14:50:20 +0300

    check brackets

--- src/index.js
+++ src/index.js
@@ -14,5 +14,14 @@ module.exports = function check(str, bracketsConfig) {
    function delString(str, start, end) {
         return newString = str.substring(0, start) + str.substring(end + 1);
     }
-    
+    var length = str.length;
+    for (var i = 0; i < length - 1; i++) {
+        if (str[i + 1] == findCloseBracket(str[i], bracketsConfig)) {
+            str = delString(str, i, i + 1);
+            i = -1;
+            length = length - 2;
+        }
+    }
+    if (str.length == 0) return true;
+    else return false;
 }
